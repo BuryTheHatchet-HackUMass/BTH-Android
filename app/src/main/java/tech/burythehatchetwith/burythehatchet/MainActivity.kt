a95af695ecc0bd87b8c6c97103bc4ae684ecfc13
AakashKB <aakashkbalaji@gmail.com>
2017-11-04 19:26:03 -0400

    Started data mapping topics

--- app/src/main/java/tech/burythehatchetwith/burythehatchet/MainActivity.kt
+++ app/src/main/java/tech/burythehatchetwith/burythehatchet/MainActivity.kt
@@ -6,16 +6,31 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.View
 import com.beust.klaxon.*
 import com.beust.klaxon.JsonArray
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_submission.*
 import org.json.JSONArray
 
 private var linearLayoutManager: LinearLayoutManager? = null
 
 class MainActivity : AppCompatActivity() {
+
+    data class userThread(val id: Long,
+                      val title: String,
+                      val expiration: Int,
+                      val tags: ArrayList<String>,
+                      val side1: String,
+                      val side2: String,
+                      val image: String)
+
+
+    var threadModel = mutableListOf<MainActivity.userThread>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +39,12 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = linearLayoutManager
 
         val font = FontUtil.get("Helvetica.ttc", this)
+        val dataEndpoint : String = "http://165.227.176.116:8080/threads"
         FontUtil.overrideFonts(findViewById(android.R.id.content), -1.0f, font, null,  null)
-        getTopics("http://165.227.176.116:8080/threads")
+        getTopics(dataEndpoint)
         //println(topics)
 
-        startActivity(Intent(this, SubmissionActivity::class.java))
+        //startActivity(Intent(this, SubmissionActivity::class.java))
 
     }
 
@@ -42,24 +58,30 @@ class MainActivity : AppCompatActivity() {
                     Log.e("HTTP Request", "Failure")
                 }is Result.Success ->{
                     Log.e("HTTP Request", "Success")
-                    serverResponse = result.get()
+                    var topics: JsonArray<JsonObject> = parseString(result.get())
+                    jsonToDm(topics)
+
                 }
 
             }
 
         }
-        if(serverResponse != "-1"){
-            var topics: JsonArray<JsonObject> = parseString(serverResponse)
-        }else{
-            println("Server error, topic not requested")
-        }
-
     }
-
 
     fun parseString(json : String) : JsonArray<JsonObject>{
         val parser: Parser = Parser()
         val stringBuilder: StringBuilder = StringBuilder(json)
         return parser.parse(stringBuilder) as JsonArray<JsonObject>
+    }
+
+    fun jsonToDm(jarr : JsonArray<JsonObject>){
+        val mapper = jacksonObjectMapper()
+        threadModel.clear()
+        (0 until jarr.size)
+                .map { jarr.get(it) }
+                //.forEach { threadModel.add(mapper.readValue<MainActivity.userThread>(it.toString())) }
+
+       // println(threadModel.get(0).title)
+        Log.e("Threads", threadModel.size.toString())
     }
 }
