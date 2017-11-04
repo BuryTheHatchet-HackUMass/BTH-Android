9c0da48337557a4e1fcb6297ce5e527d900533c9
tpatel1781 <tpatel1781@gmail.com>
2017-11-04 13:25:08 -0400

    Started working on layouts.

--- app/src/main/java/tech/burythehatchetwith/burythehatchet/MainActivity.kt
+++ app/src/main/java/tech/burythehatchetwith/burythehatchet/MainActivity.kt
@@ -2,16 +2,24 @@ package tech.burythehatchetwith.burythehatchet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
 import java.net.URL
+
+private lateinit var linearLayoutManager: LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
 
+        val font = FontUtil.get("Helvetica.ttc", this)
+        FontUtil.overrideFonts(findViewById(android.R.id.content), -1.0f, font, null,  null)
 
     }
 
@@ -27,5 +35,7 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+}
 
 }
--- app/src/main/java/tech/burythehatchetwith/burythehatchet/POJOs.kt
+++ app/src/main/java/tech/burythehatchetwith/burythehatchet/POJOs.kt
@@ -4,6 +4,12 @@ package tech.burythehatchetwith.burythehatchet
  * A representation of a POJOs which is displayed to the user
  * @author Aaron Vontell
  */
-data class Topic(val ident: Int, val surname: String, val id: String)
-data class Tag(val ident: Int, val surname: String, val id: String)
-data class Post(val ident: Int, val surname: String, val id: String)
+data class Thread(val id: Long,
+                  val title: String,
+                  val expiration: Int,
+                  val tags: ArrayList<String>,
+                  val side1: String,
+                  val side2: String,
+                  val numSize1: Int,
+                  val numSize2: Int,
+                  val image: String)
