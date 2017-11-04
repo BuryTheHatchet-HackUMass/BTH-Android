b8b510f0851d1ca77f58c5857436999140c7818e
vontell <vontell@mit.edu>
2017-11-04 18:59:13 -0400

    Add facts loader

--- app/src/main/java/tech/burythehatchetwith/burythehatchet/SubmissionActivity.kt
+++ app/src/main/java/tech/burythehatchetwith/burythehatchet/SubmissionActivity.kt
@@ -6,11 +6,9 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
-import com.beust.klaxon.JsonObject
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result
 import kotlinx.android.synthetic.main.activity_submission.*
 import org.jetbrains.anko.doAsync
@@ -27,7 +25,8 @@ class SubmissionActivity : AppCompatActivity() {
 
     val factEndpoint : String = "165.227.176.116:8080/factchecker"
     val keywordEndpoint : String = "165.227.176.116:8080/keywords"
-    var lastType = 0L
+    var hash = 0
+    private var dataReceived : JSONArray? = null
     var factModel = mutableListOf<Fact>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,11 +42,7 @@ class SubmissionActivity : AppCompatActivity() {
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
-                val now = System.currentTimeMillis();
-                //if (now - lastType > 700) {
-                    getFacts("http://165.227.176.116:8080/factchecker", p0.toString())
-                //}
-                lastType = now
+                getFacts("http://165.227.176.116:8080/factchecker", p0.toString())
 
             }
 
@@ -55,19 +50,34 @@ class SubmissionActivity : AppCompatActivity() {
 
     }
 
+    fun displayData() {
+
+        val mapper = jacksonObjectMapper()
+
+        var jarr = dataReceived
+        factModel.clear()
+        (0 until jarr!!.length())
+            .map { jarr.get(it) }
+            .forEach { factModel.add(mapper.readValue<Fact>(it.toString())) }
+
+    }
+
     fun getFacts(URL : String, body: String) {
 
-        doAsync {
+        Fuel.post(URL, listOf("content" to body)).responseString { request, response, result ->
+            when(result) {
+                is Result.Failure ->{
+                    //Log.e("HTTP Request","Failed")
+                    //Log.e("REsult", result.toString())
+                } is Result.Success ->{
+                //Log.e("HTTP Request", "Success")
+                val serverResponse = result.get()
+                val jarr = JSONArray(serverResponse)
 
-            Fuel.post(URL, listOf("content" to body)).responseString { request, response, result ->
-                when(result) {
-                    is Result.Failure ->{
-                        //Log.e("HTTP Request","Failed")
-                        //Log.e("REsult", result.toString())
-                    } is Result.Success ->{
-                    //Log.e("HTTP Request", "Success")
-                    var serverResponse = result.get()
-                    var jarr = JSONArray(serverResponse)
+                val last = jarr.hashCode()
+                if (last != hash) {
+
+                    hash = last
 
                     // Now change the UI / underlying model
                     sourcesOverview.visibility = View.VISIBLE;
@@ -77,26 +87,11 @@ class SubmissionActivity : AppCompatActivity() {
                         sourcesOverview.text = "${jarr.length()} sources available"
                     }
 
-                    Log.e("RESP", serverResponse);
-
-                    val mapper = jacksonObjectMapper()
-
-                    factModel.clear()
-                    (0 until jarr.length())
-                            .map { jarr.get(it) }
-                            .forEach { factModel.add(mapper.readValue<Fact>(it.toString())) }
-
-                    Log.e("FACTS", factModel.size.toString());
-
-                    uiThread {
-                        Log.e("DONE", "Yeah boi")
-                    }
-
                 }
+
                 }
             }
-
         }
-    }
 
+    }
 }
